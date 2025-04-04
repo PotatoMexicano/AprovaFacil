29a5cf49edf6306130ef5ed749ccf9995a1a39ad
Potato Mexicano <potatomexicano006@yahoo.com>
2025-04-03 23:26:46 -0300

    fix: Correção DTOs.
    * Ajuste no DTO de solicitação, obtendo valores da empresa.
    * Mapeamento dos DTOs de conversão para RequestDirector e RequestManager.

--- AprovaFacil.Domain/Extensions/RequestExtensions.cs
+++ AprovaFacil.Domain/Extensions/RequestExtensions.cs
@@ -31,39 +31,17 @@ public static class RequestExtensions
                 CNPJ = request.Company.CNPJ,
                 Phone = request.Company.Phone,
                 Email = request.Company.Email,
+                City = request.Company.Address.City,
+                Complement = request.Company.Address.Complement,
+                Neighborhood = request.Company.Address.Neighborhood,
+                Number = request.Company.Address.Number,
+                PostalCode = request.Company.Address.PostalCode,
+                State = request.Company.Address.State,
+                Street = request.Company.Address.Street
             },
-            Requester = new UserDTO
-            {
-                Department = request.Requester.Department,
-                Email = request.Requester.Email,
-                FullName = request.Requester.FullName,
-                Id = request.Requester.Id,
-                Role = request.Requester.Role,
-                PictureUrl = request.Requester.PictureUrl,
-                Enabled = request.Requester.Enabled,
-            },
-            Managers = [.. request.Managers.Select(x => new UserDTO
-            {
-                Department = x.User.Department,
-                Email = x.User.Email,
-                FullName = x.User.FullName,
-                Id = x.User.Id,
-                Role = x.User.Role,
-                PictureUrl = x.User.PictureUrl,
-                Enabled = x.User.Enabled,
-                RequestApproved = x.Approved,
-            })],
-            Directors = [.. request.Directors.Select(x => new UserDTO
-            {
-                Department = x.User.Department,
-                Email = x.User.Email,
-                FullName = x.User.FullName,
-                Id = x.User.Id,
-                Role = x.User.Role,
-                PictureUrl = x.User.PictureUrl,
-                Enabled = x.User.Enabled,
-                RequestApproved = x.Approved,
-            })],
+            Requester = request.Requester.ToDTO(),
+            Managers = [.. request.Managers.Select(x => x.ToDTO())],
+            Directors = [.. request.Directors.Select(x => x.ToDTO())],
         };
     }
 
--- AprovaFacil.Domain/Extensions/UserExtensions.cs
+++ AprovaFacil.Domain/Extensions/UserExtensions.cs
@@ -41,6 +41,78 @@ public static class UserExtensions
         };
     }
 
+    public static UserDTO ToDTO(this RequestDirector appUser, IEnumerable<String>? roles = default)
+    {
+        return new UserDTO
+        {
+            Id = appUser.User.Id,
+            Email = appUser.User.Email,
+            FullName = appUser.User.FullName,
+            Role = appUser.User.Role,
+            IdentityRoles = roles?.ToList() ?? [],
+            RoleLabel = appUser.User.Role switch
+            {
+                Roles.Requester => "Requisitante",
+                Roles.Manager => "Gerente",
+                Roles.Director => "Diretor",
+                Roles.Assistant => "Assistente",
+                Roles.Finance => "Financeiro",
+                _ => "Desconhecido"
+            },
+            Department = appUser.User.Department,
+            DepartmentLabel = appUser.User.Department switch
+            {
+                Departaments.Engineer => "Engenharia",
+                Departaments.Sales => "Vendas",
+                Departaments.Operations => "Operações",
+                Departaments.Marketing => "Marketing",
+                Departaments.IT => "Tecnologia da Informação",
+                Departaments.HR => "Recursos Humanos",
+                Departaments.Finance => "Financeiro",
+                _ => "Desconhecido"
+            },
+            PictureUrl = appUser.User.PictureUrl,
+            Enabled = appUser.User.Enabled,
+            RequestApproved = appUser.Approved,
+        };
+    }
+
+    public static UserDTO ToDTO(this RequestManager appUser, IEnumerable<String>? roles = default)
+    {
+        return new UserDTO
+        {
+            Id = appUser.User.Id,
+            Email = appUser.User.Email,
+            FullName = appUser.User.FullName,
+            Role = appUser.User.Role,
+            IdentityRoles = roles?.ToList() ?? [],
+            RoleLabel = appUser.User.Role switch
+            {
+                Roles.Requester => "Requisitante",
+                Roles.Manager => "Gerente",
+                Roles.Director => "Diretor",
+                Roles.Assistant => "Assistente",
+                Roles.Finance => "Financeiro",
+                _ => "Desconhecido"
+            },
+            Department = appUser.User.Department,
+            DepartmentLabel = appUser.User.Department switch
+            {
+                Departaments.Engineer => "Engenharia",
+                Departaments.Sales => "Vendas",
+                Departaments.Operations => "Operações",
+                Departaments.Marketing => "Marketing",
+                Departaments.IT => "Tecnologia da Informação",
+                Departaments.HR => "Recursos Humanos",
+                Departaments.Finance => "Financeiro",
+                _ => "Desconhecido"
+            },
+            PictureUrl = appUser.User.PictureUrl,
+            Enabled = appUser.User.Enabled,
+            RequestApproved = appUser.Approved,
+        };
+    }
+
     public static IApplicationUser Update(ref IApplicationUser user, UserUpdateDTO request)
     {
         user.UserName = request.Email;
